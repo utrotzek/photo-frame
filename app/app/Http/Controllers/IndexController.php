228b36bd58453a7203064b6ad85898d1c94df5a6
Uwe Trotzek <uwe.trotzek@gmail.com>
2020-12-27 23:31:04 +0100

    [TASK] Improve initialization of index_status

--- app/app/Http/Controllers/IndexController.php
+++ app/app/Http/Controllers/IndexController.php
@@ -13,17 +13,11 @@ class IndexController extends Controller
      * @return Response
      */
     public function state() {
-        try {
-            return new Response(
-                new IndexStateResource(
-                    IndexState::query()->findOrFail(1)->get()->first()
-                )
-            );
-        } catch (\Exception $exception) {
-            $state = new IndexState(['state' => 'waiting']);
-            $state->save();
-            return new Response($state->fresh());
-        }
+        return new Response(
+            new IndexStateResource(
+                IndexState::query()->findOrFail(1)->get()->first()
+            )
+        );
     }
 
     public function update(Request $request ) {
--- app/database/migrations/2020_12_16_201153_create_index_states.php
+++ app/database/migrations/2020_12_16_201153_create_index_states.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateIndexStates extends Migration
@@ -23,6 +24,12 @@ class CreateIndexStates extends Migration
             $table->dateTime('last_run')->useCurrent();
             $table->timestamps();
         });
+
+        DB::table('index_states')->insert(
+            [
+                'state' => \App\Models\IndexState::STATE_WAITING,
+            ]
+        );
     }
 
     /**
